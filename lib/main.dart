3b888dab6062392721a0fdf6ea6ec2c237639318
rajpate1225 <rkumar79441@gmail.com>
2021-01-29 15:07:38 +0530

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,59 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': "What's your favourite color?",
-      'answers': ['blue', 'black', 'green', 'red'],
+      'questionText':
+          "India is a federal union comprising twenty-nine states and how many union territories?",
+      'answers': [
+        {'text': '6', 'score': 0},
+        {'text': '7', 'score': 1},
+        {'text': '8', 'score': 0},
+        {'text': '9', 'score': 0},
+      ],
     },
     {
-      'questionText': "What's your favourite animal?",
-      'answers': ['dog', 'cat', 'elephant', 'zebra'],
+      'questionText':
+          "Which of the following is the capital of Arunachal Pradesh?",
+      'answers': [
+        {'text': 'Itanagar', 'score': 1},
+        {'text': 'Dispur', 'score': 0},
+        {'text': 'Imphal', 'score': 0},
+        {'text': 'Panaji', 'score': 0},
+      ],
     },
     {
-      'questionText': "What's your favourite hobby?",
-      'answers': ['read', 'dance', 'sing', 'movies'],
+      'questionText': "What are the major languages spoken in Andhra Pradesh?",
+      'answers': [
+        {'text': 'English and Telugu', 'score': 0},
+        {'text': 'Telugu and Urdu', 'score': 1},
+        {'text': 'Telugu and Kannada', 'score': 0},
+        {'text': 'All of the above languages', 'score': 0},
+      ],
+    },
+    {
+      'questionText': "What is the state flower of Haryana?",
+      'answers': [
+        {'text': 'Lotus', 'score': 1},
+        {'text': 'Rhododendron', 'score': 0},
+        {'text': 'Golden Shower', 'score': 0},
+        {'text': 'Not declared', 'score': 0},
+      ],
+    },
+    {
+      'questionText':
+          "Which of the following states is not located in the North?",
+      'answers': [
+        {'text': 'Jharkhand', 'score': 1},
+        {'text': 'Jammu and Kashmir', 'score': 0},
+        {'text': 'Himachal Pradesh', 'score': 0},
+        {'text': 'Haryana', 'score': 0},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestions() {
+  void _answerQuestions(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex += 1;
     });
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -18,8 +18,9 @@ class Quiz extends StatelessWidget {
           questions[questionIndex]
               ['questionText'], //use [index] instead of elementsAt
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
         }).toList()
       ],
     );
